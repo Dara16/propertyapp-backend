f30151a09ed852d560733df226a2850bbf946c57
Dara16 <just4oludee@yahoo.com>
2022-08-08 00:28:10 -0400

    db seeded

--- app/models/occupant.rb
+++ app/models/occupant.rb
@@ -1,2 +1,4 @@
 class Occupant < ApplicationRecord
+    belongs_to :apartment
+    belongs_to :tenement
 end
--- app/models/tenement.rb
+++ app/models/tenement.rb
@@ -1,2 +1,4 @@
 class Tenement < ApplicationRecord
+    has_many :apartments
+    has_many :occupants, through: :apartments
 end
