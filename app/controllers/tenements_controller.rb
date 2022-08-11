532d337fade97f5c248a1d16d5eba68ac91368e6
Dara16 <just4oludee@yahoo.com>
2022-08-10 23:45:18 -0400

    controller actions for tenement resource

--- app/controllers/tenements_controller.rb
+++ app/controllers/tenements_controller.rb
@@ -1,6 +1,40 @@
 class TenementsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
    def index
         tenements = Tenement.all
         render json: tenements
+   end
+
+   def show
+        tenement = Tenement.find(params[:id])
+        render json: tenement
+   end
+
+   def create
+        tenement = Tenement.create(tenement_params)
+        render json: tenement
+   end
+
+   def update
+        tenement = Tenement.find(params[:id])
+        tenement.update(tenement_params)
+        render json: tenement
+   end
+
+   def destroy
+        tenement = Tenement.find(params[:id])
+        tenement.destroy
+        render json: tenement
+   end
+
+   private
+
+   def tenement_params
+        params.require(:tenement).permit(:name)
+   end
+
+   def render_not_found_response
+        render json: { errors: 'Building Not Found' }, 
+        status: :not_found
    end
 end
