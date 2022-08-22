53501576e20eb53405f4548036ca29354808600e
Dara16 <just4oludee@yahoo.com>
2022-08-22 07:23:04 -0400

    show route added to apartments

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,8 +1,22 @@
 class ApartmentsController < ApplicationController
-    def index
+     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+     def index
          apartments = Apartment.all
          render json: apartments
-    end
+     end
 
+     def show
+          id = params[:id]
+          course = Apartment.find(id)
+          render json: course, except: [:created_at, :updated_at]
+     end
+
+     private
+
+     def render_not_found_response
+          render json: {error: "Apartment not found "},
+          status: :not_found
+     end
     
  end
